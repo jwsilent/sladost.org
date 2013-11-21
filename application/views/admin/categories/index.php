a24897bbb52bbb4a7ebbac0dbfd05890d510c1f4
nocool <john.loudless@gmail.com>
2013-11-21 13:31:31 +0400

    admin in progress #2

--- application/views/admin/categories/index.php
+++ application/views/admin/categories/index.php
@@ -1,6 +1,20 @@
 <div class="crud_links">
 	<a class="btn" href="categories/new">Создать</a>
 </div>
+
+<div class="span11">
+	<div class="story_item" style="background:none;">
+		<span >Наименование</span>
+<!-- 		<div class="story_item_controls">
+			<p><a href="categories/<?php echo $category['id'] ?>/edit">Редактировать</a></p>
+			<p><a href="categories/<?php echo $category['id'] ?>/delete">Удалить</a></p>
+		</div> -->
+	</div>
+</div>
+<br>
+<br>
+<br>
+
 <?php foreach ($categories as $category): ?>
 
 <div class="span11">
